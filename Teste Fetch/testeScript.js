6f4b65ef90d10e8cacb1cda6f8d9d97ea4050914
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2023-03-08 00:21:37 -0300

    Aprendi a usar o Fetch :)

--- Teste Fetch/testeScript.js
+++ Teste Fetch/testeScript.js
@@ -1,19 +1,35 @@
-fetch("https://aawz-public.s3.us-east-2.amazonaws.com/live-coding-front/dados-desafio.json")
+
+function puxarDados(){
+    
+    fetch("https://aawz-public.s3.us-east-2.amazonaws.com/live-coding-front/dados-desafio.json")
     .then(r => r.json())
     .then(jsonBody =>{
         testeShow(jsonBody)
     })
 
+    const cabecalho = document.querySelector('#cabecalho')
+    const tabela = document.querySelector('#tabela')
 
-function testeShow(data){
-    const keys = Object.keys(data[0])
+    function testeShow(data){
+        const keys = Object.keys(data[0])
+
+        keys.forEach(e =>{
+            let celula = document.createElement('th')
+            celula.innerText = e
+            tabela.appendChild(celula)
+        })
+
+        data.forEach(e =>{
+            let linha = document.createElement('tr')
+            keys.forEach(key=>{
+                let cel = document.createElement('td')
+                cel.innerText = e[key]
+                linha.appendChild(cel)
+            })
+            tabela.appendChild(linha)
+        })
+    }
+
 }
 
-function showTable(){
-    const cabecalho = document.createElement('div')
-    keys.forEach(item=>{
-        let firstLine = document.createElement('th')
-        firstLine.textContent = item
-        cabecalho.appendChild(firstLine)
-      })
-}
+
